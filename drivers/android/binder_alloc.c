d9a2e194b8fea4ae33dbb6a703a2e983c914358b
utziacre <utziacre@gmail.com>
2024-03-12 22:12:00 +0800

    drivers: Import xiaomi millet
    Signed-off-by: Xirudly <734428735@qq.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -32,6 +32,9 @@
 #include <linux/highmem.h>
 #include "binder_alloc.h"
 #include "binder_trace.h"
+#if IS_ENABLED(CONFIG_MILLET)
+#include <linux/millet.h>
+#endif
 
 struct list_lru binder_alloc_lru;
 
@@ -387,6 +390,10 @@ static bool debug_low_async_space_locked(struct binder_alloc *alloc, int pid)
 	return false;
 }
 
+#if IS_ENABLED(CONFIG_MILLET)
+extern struct task_struct *binder_buff_owner(struct binder_alloc *alloc);
+#endif
+
 static struct binder_buffer *binder_alloc_new_buf_locked(
 				struct binder_alloc *alloc,
 				size_t data_size,
@@ -436,6 +443,27 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 			      alloc->pid, size);
 		return ERR_PTR(-ENOSPC);
 	}
+
+#if IS_ENABLED(CONFIG_MILLET)
+	if (is_async
+		&& (alloc->free_async_space
+			< WARN_AHEAD_MSGS * (size + sizeof(struct binder_buffer))
+			|| alloc->free_async_space < binder_warn_ahead_space)) {
+			struct millet_data data;
+			struct task_struct *owner;
+
+			owner = binder_buff_owner(alloc);
+			if (owner) {
+				memset(&data, 0, sizeof(struct millet_data));
+				data.pri[0] =  BINDER_BUFF_WARN;
+				data.mod.k_priv.binder.trans.dst_task = owner;
+				data.mod.k_priv.binder.trans.src_task = current;
+				millet_sendmsg(BINDER_TYPE, owner, &data);
+			}
+	}
+	if (false)
+		binder_alloc_debug(BINDER_DEBUG_BUFFER_ALLOC, "%s", NAME_ARRAY[0]);
+#endif
 
 	while (n) {
 		buffer = rb_entry(n, struct binder_buffer, rb_node);
