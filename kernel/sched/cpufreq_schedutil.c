7affd8cc5f98f484baaa68546ddfd6a82a09a4e7
diudiuyc5200 <17766728994@163.com>
2026-09-03 23:10:19 +0800

    优化

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -427,8 +427,8 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 		 * 针对大核（CPU >= 4）的轻负载快速升频
 		 * 如果 util 超过阈值，直接跳到最大频率的 70%
 		 */
-		if (policy->cpu == 7 && util > 0) {
-			unsigned int target = policy->max * 85 / 100;
+		if (policy->cpu == 7 && util > 5) {
+			unsigned int target = policy->max * 80 / 100;
 			if (next_f < target)
 				next_f = target;
 		}
@@ -860,10 +860,10 @@ static int sugov_init(struct cpufreq_policy *policy)
 
 	if (policy->cpu >= 4) {
 		tunables->up_rate_limit_us = 500;
-		tunables->down_rate_limit_us = 50000;  /* 60ms，降频慢 */
+		tunables->down_rate_limit_us = 500;  /* 60ms，降频慢 */
 	} else {
 		tunables->up_rate_limit_us = 500;
-		tunables->down_rate_limit_us = 50000;   /* 5ms，降频快 */
+		tunables->down_rate_limit_us = 500;   /* 5ms，降频快 */
 	}
 
 	policy->governor_data = sg_policy;
