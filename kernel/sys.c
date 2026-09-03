cca8a910f9140dd16a9555cd7be8405844e17ecb
diudiuyc5200 <17766728994@163.com>
2026-09-03 12:17:12 +0800

    Revert "fs: Import inline hooks"
    This reverts commit 476ddbb3bf50e198792cff060dd1292ac637dab7.

--- kernel/sys.c
+++ kernel/sys.c
@@ -590,9 +590,6 @@ error:
 	return retval;
 }
 
-#ifdef CONFIG_KSU_SUSFS
-extern int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid);
-#endif
 
 /*
  * This function implements a generic ability to update ruid, euid,
@@ -605,9 +602,7 @@ SYSCALL_DEFINE3(setresuid, uid_t, ruid, uid_t, euid, uid_t, suid)
 	struct cred *new;
 	int retval;
 	kuid_t kruid, keuid, ksuid;
-#ifdef CONFIG_KSU_SUSFS
-       (void)ksu_handle_setresuid(ruid, euid, suid);
-#endif
+
 	kruid = make_kuid(ns, ruid);
 	keuid = make_kuid(ns, euid);
 	ksuid = make_kuid(ns, suid);
