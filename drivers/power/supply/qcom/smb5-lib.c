57d3b52d45a640b14261704e4e5245334a63a158
diudiuyc5200 <17766728994@163.com>
2026-09-13 21:23:25 +0800

    修复部分警告

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -5162,11 +5162,8 @@ int smblib_get_prop_smb_health(struct smb_charger *chg)
 }
 
 #define SDP_CURRENT_UA			500000
-#define CDP_CURRENT_UA			1500000
 #define HVDCP_START_CURRENT_UA		1000000
 #define TYPEC_DEFAULT_CURRENT_UA	900000
-#define TYPEC_MEDIUM_CURRENT_UA		1500000
-#define TYPEC_HIGH_CURRENT_UA		3000000
 
 int smblib_get_prop_die_health(struct smb_charger *chg)
 {
@@ -5327,8 +5324,6 @@ int smblib_get_prop_connector_temp(struct smb_charger *chg)
 
 #define SDP_CURRENT_UA			500000
 #define CDP_CURRENT_UA			1500000
-#define DCP_CURRENT_UA			1600000
-#define HVDCP_CURRENT_UA		2800000
 #define HVDCP_START_CURRENT_UA		1000000
 #define TYPEC_DEFAULT_CURRENT_UA	900000
 #define TYPEC_MEDIUM_CURRENT_UA		1500000
