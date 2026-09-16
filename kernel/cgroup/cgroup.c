65b373927acc7173d474a03a4d419247b7708455
diudiuyc5200 <17766728994@163.com>
2026-09-16 21:49:29 +0800

    Revert "UPSTREAM: cgroupfs: Support user xattrs"
    This reverts commit c327eb4e336713d9ce41a00f01af20105ff1a6fb.

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -1998,8 +1998,7 @@ int cgroup_setup_root(struct cgroup_root *root, u16 ss_mask)
 
 	root->kf_root = kernfs_create_root(kf_sops,
 					   KERNFS_ROOT_CREATE_DEACTIVATED |
-					   KERNFS_ROOT_SUPPORT_EXPORTOP |
-					   KERNFS_ROOT_SUPPORT_USER_XATTR,
+					   KERNFS_ROOT_SUPPORT_EXPORTOP,
 					   root_cgrp);
 	if (IS_ERR(root->kf_root)) {
 		ret = PTR_ERR(root->kf_root);
